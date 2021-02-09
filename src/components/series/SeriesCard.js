f98e2602e9da5b863decdc13218ee0cb01563287
cjcjon <cjcjon30@gmail.com>
2021-02-10 02:02:17 +0900

    feat: Add skeleton ui feature to SeriesCard

--- src/components/series/SeriesCard.js
+++ src/components/series/SeriesCard.js
@@ -6,6 +6,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import HistoryIcon from "@material-ui/icons/History";
+import Skeleton from "@material-ui/lab/Skeleton";
 import LinkWrapper from "@components/links/LinkWrapper";
 import { useWordStyles } from "@styles/useful.styles";
 
@@ -35,50 +36,81 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function SeriesCard({ href, title, number, date, recent }) {
+function SeriesCard({ loading, href, title, number, date, recent }) {
   const classes = useStyles();
   const wordStyles = useWordStyles();
 
   return (
-    <LinkWrapper href={href}>
-      <Card className={classes.root} elevation={2}>
-        <CardMedia
-          image="https://source.unsplash.com/random"
-          alt="we don't have image"
-          className={classes.cardMedia}
-        />
-        <div className={classes.cardContent}>
-          <Typography variant="body2" color="textSecondary">
-            {number}개의 포스트
-          </Typography>
-          <Typography gutterBottom variant="h6" className={wordStyles.bold}>
-            {title}
-          </Typography>
-        </div>
-        <Divider />
-        <Grid container className={classes.cardBottom}>
-          <Grid item xs>
-            <Typography variant="body2" color="textSecondary">
-              {date}
-            </Typography>
-          </Grid>
-          {recent && (
-            <Grid
-              container
-              item
-              xs
-              alignItems="center"
-              wrap="wrap"
-              justify="flex-end"
-              className={classes.updated}
-            >
-              Updated&nbsp;
-              <HistoryIcon />
+    <>
+      {loading ? (
+        <Card className={classes.root} elevation={2}>
+          <Skeleton
+            variant="rect"
+            width="100%"
+            animation="wave"
+            className={classes.cardMedia}
+          />
+          <div className={classes.cardContent}>
+            <Skeleton width="100%" animation="wave">
+              <Typography variant="body2">.</Typography>
+            </Skeleton>
+            <Skeleton width="100%" animation="wave">
+              <Typography gutterBottom variant="h6">
+                .
+              </Typography>
+            </Skeleton>
+          </div>
+          <Divider />
+          <Grid container className={classes.cardBottom}>
+            <Grid item xs>
+              <Skeleton width="100%" animation="wave">
+                <Typography variant="body2">.</Typography>
+              </Skeleton>
             </Grid>
-          )}
-        </Grid>
-      </Card>
-    </LinkWrapper>
+          </Grid>
+        </Card>
+      ) : (
+        <LinkWrapper href={href}>
+          <Card className={classes.root} elevation={2}>
+            <CardMedia
+              image="https://source.unsplash.com/random"
+              alt="we don't have image"
+              className={classes.cardMedia}
+            />
+            <div className={classes.cardContent}>
+              <Typography variant="body2" color="textSecondary">
+                {number}개의 포스트
+              </Typography>
+              <Typography gutterBottom variant="h6" className={wordStyles.bold}>
+                {title}
+              </Typography>
+            </div>
+            <Divider />
+            <Grid container className={classes.cardBottom}>
+              <Grid item xs>
+                <Typography variant="body2" color="textSecondary">
+                  {date}
+                </Typography>
+              </Grid>
+              {recent && (
+                <Grid
+                  container
+                  item
+                  xs
+                  alignItems="center"
+                  wrap="wrap"
+                  justify="flex-end"
+                  className={classes.updated}
+                >
+                  Updated&nbsp;
+                  <HistoryIcon />
+                </Grid>
+              )}
+            </Grid>
+          </Card>
+        </LinkWrapper>
+      )}
+    </>
   );
 }
 
