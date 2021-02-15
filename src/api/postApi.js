3c58353e2ded778e282d4707ea2247c1f43443bb
cjcjon <cjcjon30@gmail.com>
2021-02-15 19:52:41 +0900

    feat: Add upload, delete post image api

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,5 +1,8 @@
 import configedAxios from "../axios";
 
+// delete와 get은 body에 데이터가 들어가지 않는다
+// 두 번째 인자에 data: {} attribute 넣으면 해결
+
 const recentPost = () => {
   return configedAxios.get("/api/post/recent");
 };
@@ -12,10 +15,20 @@ const mostViewPost = () => {
   return configedAxios.get("/api/post/views");
 };
 
+const uploadImage = (formData) => {
+  return configedAxios.post("/api/post/image", formData);
+};
+
+const deleteImage = (imageName) => {
+  return configedAxios.delete(`/api/post/image/${imageName}`);
+};
+
 const postApi = {
   recentPost,
   recommandPost,
   mostViewPost,
+  uploadImage,
+  deleteImage,
 };
 
 export default postApi;
